46aaeddb1e86023d4cfc1c737062c46457a4dbdf
Sourabh Jadhav <sourabh.jadhav01@techenvo.com>
2025-09-12 12:33:38 +0530

    initial commit

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -6,19 +6,27 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws Exception {
         ChromeOptions options = new ChromeOptions();
-        // comment out headless for visible mode
-        // options.addArguments("--headless=new");
+
+        // Headless for CI
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+
+        // ✅ Use a unique temporary user-data-dir to avoid session conflicts
+        Path tempDir = Files.createTempDirectory("chrome-user-data");
+        options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath());
+
         driver = new ChromeDriver(options);
     }
-
 
     @AfterMethod
     public void tearDown() {
@@ -27,7 +35,6 @@ public class BaseTest {
         }
     }
 
-    // ✅ Getter for TestListener
     public WebDriver getDriver() {
         return driver;
     }
