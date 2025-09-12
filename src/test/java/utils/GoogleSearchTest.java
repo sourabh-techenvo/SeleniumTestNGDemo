44cf751b49043b8193751dea2ea282695c71853c
Sourabh Jadhav <sourabh.jadhav01@techenvo.com>
2025-09-12 21:05:26 +0530

    initial commit

--- src/test/java/utils/GoogleSearchTest.java
+++ src/test/java/utils/GoogleSearchTest.java
@@ -16,17 +16,17 @@ public class GoogleSearchTest extends BaseTest {
 	}
 
 	
-    @Test
-    public void testGoogleSearch() {
-        driver.get("https://www.google.com");
-
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium WebDriver");
-        searchBox.submit();
-
-        // validation
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Selenium"),
-                "Expected title to contain 'Selenium' but got: " + title);
-    }
+//    @Test
+//    public void testGoogleSearch() {
+//        driver.get("https://www.google.com");
+//
+//        WebElement searchBox = driver.findElement(By.name("q"));
+//        searchBox.sendKeys("Selenium WebDriver");
+//        searchBox.submit();
+//
+//        // validation
+//        String title = driver.getTitle();
+//        Assert.assertTrue(title.contains("Selenium"),
+//                "Expected title to contain 'Selenium' but got: " + title);
+//    }
 }
